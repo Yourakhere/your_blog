d56b0f1907124d6e1ebc11a733da04ec3750c906
Abhishek kumar <139471330+Yourakhere@users.noreply.github.com>
2025-04-16 10:41:15 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
 
     try {
       const { data } = await axios.post(
-        "http://localhost:4001/api/users/login",
+        "https://your-blog-alpha.vercel.app/api/users/login",
         { email, password, role },
         {
           withCredentials: true,
